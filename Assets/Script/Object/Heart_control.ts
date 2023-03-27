8cc5bba2747bf3e79d7edbf3d6b3f420c07f916b
Todah01 <57752105+Todah01@users.noreply.github.com>
2023-03-27 18:21:47 +0900

    2023.03.27
    1. Add get coin sound in heart control logic by using AudioManager.
    
    2. Fix game retry logic and continue logic

--- Assets/Script/Object/Heart_control.ts
+++ Assets/Script/Object/Heart_control.ts
@@ -1,8 +1,9 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import { Object, Collider, Collision, Debug, GameObject, LayerMask, ParticleSystem } from 'UnityEngine';
+import { Object, Collider, Collision, Debug, GameObject, LayerMask, ParticleSystem, AudioClip, Resources } from 'UnityEngine';
 import { Text } from 'UnityEngine.UI';
 import { Particle } from 'UnityEngine.ParticleSystem';
 import MainData from '../Data/MainData';
+import AudioManager from '../Character/AudioManager';
 
 export default class Heart_control extends ZepetoScriptBehaviour {
 
@@ -12,8 +13,17 @@ export default class Heart_control extends ZepetoScriptBehaviour {
     public cloud_anim: GameObject;
     public cloud_off: number = 0;
 
+    private getCoinClip: AudioClip;
+
+    Start() {
+        this.getCoinClip = Resources.Load<AudioClip>("Audio/SFX/CoinBlue");
+        console.log(this.getCoinClip.name);
+    }
+
     OnTriggerEnter(coll: Collider) {
         if (coll.gameObject.layer == this.player_layer) {
+            AudioManager.instance.SFXPlay("GetCoin", this.getCoinClip);
+            this.cloud_effect.SetActive(true);
             this.gameObject.SetActive(false);
             this.cloud_off = 1;
             this.CloudCntData.GetComponent<MainData>().CloudCnt += 1;
@@ -21,7 +31,6 @@ export default class Heart_control extends ZepetoScriptBehaviour {
             this.CloudCntData.GetComponent<MainData>().SetData();
             this.CloudCntData.GetComponent<MainData>().Save();
             this.CloudCntData.GetComponent<MainData>().CheckGameEnd();
-            this.cloud_effect.SetActive(true);
             // Object.Destroy(this.gameObject, 5);
         }
     }
